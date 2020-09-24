0c163d488408a8355c7865c74b9a2f45ddee87bf
Ilya <44428408+ilya-varchenya@users.noreply.github.com>
2020-09-24 13:20:36 +0300

    Update zebrunner.sh

--- zebrunner.sh
+++ zebrunner.sh
@@ -129,7 +129,7 @@ CONTAINER_NAME="sonarqube"
       	  shutdown       Stop and remove container, clear volumes
       	  backup         Backup container
       	  restore        Restore container
-          version        Version of container"
+      	  version        Version of container"
       echo_telegram
       exit 0
   }
